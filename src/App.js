e5de8c2d3ddc3f1ad79d4eaad25dbca501d1952c
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-06 02:52:32 +0200

    fix: remove zoom hack and duplicate Contact component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import { useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import ContactForm from './components/ContactForm/ContactForm';
 import HeroS from './components/Hero/HeroS';
 import SideNavbar from './components/SideNavbar/SideNavbar';
 
 function App() {
-    useEffect(() => {
-    document.body.style.zoom = "90%"; // مثلاً
-  }, []);
   return (
     <div className="font-sans text-gray-900 ">
       
@@ -21,16 +16,15 @@ function App() {
       <SideNavbar/>
       
       <HeroS id="home"/>
-      
-     
-      <About id="about"/>
-      <Skills id="skills"/>
-      <Projects id="projects"/>
-      <Contact id="contact"/> 
-      <ContactForm id="contact"/>
-      <Footer />
-      {/* {/*contact Compo in ContactForm verschiben} */}
-     
+
+      <div className="app-shell">
+        <About id="about"/>
+        <Skills id="skills"/>
+        <Projects id="projects"/>
+        <ContactForm id="contact"/>
+        <Footer />
+      </div>
+
     </div>
   );
 }
